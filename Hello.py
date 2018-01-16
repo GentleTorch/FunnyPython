e3632ad6f7b9f16d3425f4d1e191fba70e57bc1e
Leaf <JesusLeaf@163.com>
2018-01-16 17:29:11 +0800

    second commit

--- Hello.py
+++ Hello.py
@@ -1,6 +1,14 @@
 
 import numpy as np
+import random
 
 data=np.array((1,2,3,4))
 
 print data
+
+def demo_random():
+    print random.random()
+    print random.choice(range(0,100,3))
+
+if __name__=='__main__':
+    demo_random()
