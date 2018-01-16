01e2e3321492068ba9a2a055759f2c1f9c1042f1
Leaf <JesusLeaf@163.com>
2018-01-16 17:43:01 +0800

    use random function to see what happened

--- Hello.py
+++ Hello.py
@@ -7,8 +7,15 @@ data=np.array((1,2,3,4))
 print data
 
 def demo_random():
-    print random.random()
+   # print random.random()
+    data=list()
     print random.choice(range(0,100,3))
+    for i in range(0,100000):
+       data.append(random.randint(0,123))
+
+    pos=data.index(123)
+    print data[pos:pos+5]
+
 
 if __name__=='__main__':
     demo_random()
